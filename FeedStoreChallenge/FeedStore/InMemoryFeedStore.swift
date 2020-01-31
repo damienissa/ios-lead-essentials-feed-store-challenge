ea703f18787009fdd20d59f61e01eb26184bd7dc
Dmitriy Virych <dmitriy.virych@l1inc.com>
2020-01-31 15:42:26 +0200

    Implemented in memory feed store

---
+++ FeedStoreChallenge/FeedStore/InMemoryFeedStore.swift
@@ -0,0 +1,44 @@
+//
+//  InMemoryFeedStore.swift
+//  FeedStoreChallenge
+//
+//  Created by Dima Virych on 31.01.2020.
+//  Copyright © 2020 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+public final class InMemoryFeedStore {
+    
+    private struct Cache<Value> {
+        let timeStamp: Date
+        let value: [Value]
+    }
+    
+    private var cache: Cache<LocalFeedImage>?
+    
+    public init() { }
+}
+
+
+// MARK: - FeedStore
+
+extension InMemoryFeedStore: FeedStore {
+    
+    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        
+        cache = nil
+        completion(nil)
+    }
+    
+    public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+        
+        cache = Cache(timeStamp: timestamp, value: feed)
+        completion(nil)
+    }
+    
+    public func retrieve(completion: @escaping RetrievalCompletion) {
+        
+        completion(cache == nil ? .empty : .found(feed: cache!.value, timestamp: cache!.timeStamp))
+    }
+}
